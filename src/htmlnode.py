e07627a83e40679bfdde393cda0d092e68ab9b08
wjseele <wjseele@pm.me>
2025-07-02 14:03:30 +0200

    Started work on the html converter

---
+++ src/htmlnode.py
@@ -0,0 +1,20 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("Should be overridden by child")
+
+    def props_to_html(self):
+        if self.props is None:
+            raise Exception("No props to convert")
+        string_to_return = ""
+        for prop in self.props:
+            string_to_return += f" {prop.strip('"')}:{self.props[prop]}"
+        return string_to_return
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
---
+++ src/test_htmlnode.py
@@ -0,0 +1,21 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_eq(self):
+        pass
+
+    def test_not_eq(self):
+        pass
+
+    def test_prop_print(self):
+        pass
+
+    def test_raise_trouble(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
