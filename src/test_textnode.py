44d2080c52a05a0d00e935483114f75b246944eb
wjseele <wjseele@pm.me>
2025-07-02 12:06:08 +0200

    Updated test cases

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,16 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_not_eq(self):
+        nodeItalic = TextNode("This is a text node", TextType.ITALIC)
+        nodeCode = TextNode("This is a text node", TextType.CODE)
+        self.assertNotEqual(nodeItalic, nodeCode)
+
+    def test_has_url(self):
+        nodeUrl = TextNode("This is an url", TextType.LINK, "https://www.boot.dev")
+        self.assertIsNotNone(nodeUrl.url)
+        self.assertEqual(nodeUrl.url, "https://www.boot.dev")
+
 
 if __name__ == "__main__":
     unittest.main()
