eb1fb02ea74211f19249450fec0318de5e4afaed
wjseele <wjseele@pm.me>
2025-07-07 21:58:59 +0200

    Work in progress

--- src/extractor.py
+++ src/extractor.py
@@ -1,5 +1,7 @@
 import re
 
+from textnode import TextType
+
 
 def extract_markdown_images(text):
     if text is None:
@@ -13,3 +15,22 @@ def extract_markdown_links(text):
         raise ValueError("Didn't get any text")
     matches = re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
     return matches
+
+
+def split_nodes_image(old_nodes):
+    if not old_nodes:
+        raise ValueError("No nodes received")
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type is not TextType.TEXT:
+            new_nodes.append(node)
+        else:
+            images = extract_markdown_images(node)
+            if not images:
+                new_nodes.append(node)
+
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    pass
