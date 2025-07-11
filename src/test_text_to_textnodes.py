1b762d04a456d53d1ad3e2c38cb50a6475d01edc
wjseele <wjseele@pm.me>
2025-07-11 15:30:53 +0200

    Woohoo, markdown to textnodes done

---
+++ src/test_text_to_textnodes.py
@@ -0,0 +1,24 @@
+import unittest
+
+from text_to_textnodes import text_to_textnodes
+from textnode import TextNode, TextType
+
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_all_the_things(self):
+        sample = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode(
+                "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+            ),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(text_to_textnodes(sample), expected)
---
+++ src/text_to_textnodes.py
@@ -0,0 +1,18 @@
+from extractor import split_nodes_image, split_nodes_link
+from delimiter import split_nodes_delimiter
+
+from textnode import TextType, TextNode
+
+
+def text_to_textnodes(text):
+    if not text:
+        raise ValueError("Didn't receive any text")
+    delimiters = {"**": TextType.BOLD, "_": TextType.ITALIC, "`": TextType.CODE}
+    split_nodes = split_nodes_image(split_nodes_link([TextNode(text, TextType.TEXT)]))
+    for delimiter in delimiters:
+        split_nodes = split_nodes_delimiter(
+            split_nodes,
+            delimiter,
+            delimiters[delimiter],
+        )
+    return split_nodes
