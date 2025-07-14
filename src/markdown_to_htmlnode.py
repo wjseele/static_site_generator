da81a2cf3e4aadf0f48fcb25c7c57f065c24b798
wjseele <wjseele@pm.me>
2025-07-14 08:30:37 +0200

    Noodling. This still needs a lot of work

--- src/markdown_to_htmlnode.py
+++ src/markdown_to_htmlnode.py
@@ -1,4 +1,9 @@
 from block_splitter import markdown_to_blocks
+from htmlnode import ParentNode, LeafNode
+from textnode import TextNode, TextType
+from blocknode import block_to_block_type, BlockType
+from delimiter import split_nodes_delimiter
+from extractor import split_nodes_image, split_nodes_link
 
 
 def markdown_to_html_node(markdown):
@@ -8,5 +13,23 @@ def markdown_to_html_node(markdown):
         raise ValueError(f"Expected str, got {type(markdown)}")
     markdown_blocks = markdown_to_blocks(markdown)
     for block in markdown_blocks:
-        
+        if block_to_block_type(block) is BlockType.PARAGRAPH:
+            block = paragraph_to_textnodes(block)
+            block = paragraph_to_htmlnodes(block)
+    # return html_output
+
+
+def paragraph_to_textnodes(block):
+    lines = block.split("\n")
+    for line in lines:
+        line = TextNode(line, TextType.TEXT)
+    lines = split_nodes_delimiter(lines, "**", TextType.BOLD)
+    lines = split_nodes_delimiter(lines, "_", TextType.ITALIC)
+    lines = split_nodes_delimiter(lines, "`", TextType.CODE)
+    lines = split_nodes_image(lines)
+    lines = split_nodes_link(lines)
+    return lines
+
+
+def paragraph_to_htmlnodes(block):
     pass
