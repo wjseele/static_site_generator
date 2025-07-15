137b19073e56ff99d2ba18397e8dc9ba23f0628c
wjseele <wjseele@pm.me>
2025-07-15 12:45:50 +0200

    Cleaned things up some more

--- src/main.py
+++ src/main.py
@@ -4,27 +4,27 @@ import shutil
 
 def __main__():
     source = os.path.join(os.getcwd(), "static")
+    if not check_path(source):
+        raise Exception(f"Couldn't find a source path at {source}")
     destination = os.path.join(os.getcwd(), "public")
     copy_log = copy_source_files_to_destination(source, destination)
     for entry in copy_log:
         print(entry)
 
 
-def check_paths(source, destination):
-    return os.path.exists(source) and os.path.exists(destination)
+def check_path(path):
+    return os.path.exists(path)
 
 
 def clean_path(destination):
     if os.path.exists(destination):
         shutil.rmtree(destination)
-        # print(f"Removed {destination}")
     return
 
 
 def create_path(destination):
     clean_path(destination)
     os.mkdir(destination)
-    # print(f"Created {destination}")
     return
 
 
@@ -34,7 +34,7 @@ def get_folder_contents(source):
 
 
 def copy_source_files_to_destination(source, destination):
-    if not check_paths(source, destination):
+    if not check_path(destination):
         create_path(destination)
     contents = get_folder_contents(source)
     copy_log = []
@@ -43,10 +43,9 @@ def copy_source_files_to_destination(source, destination):
         destination_path = os.path.join(destination, content)
         if os.path.isfile(source_path):
             shutil.copy(source_path, destination_path)
-            # print(f"Copied {source_path} to {destination_path}")
             copy_log.append(f"Copied {source_path} to {destination_path}")
         elif os.path.isdir(source_path):
-            copy_log.append(
+            copy_log.extend(
                 copy_source_files_to_destination(source_path, destination_path)
             )
     return copy_log
