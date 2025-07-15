9d143e876dae0cd07f241a8ccf364e8dc1f5695f
wjseele <wjseele@pm.me>
2025-07-15 15:16:04 +0200

    Fixed the regex

--- src/generator.py
+++ src/generator.py
@@ -5,9 +5,7 @@ import re
 def extract_title(markdown):
     if not markdown:
         raise Exception("Didn't get any input")
-    title = re.findall(r"(#+ [a-zA-Z]+)", markdown)
+    title = re.findall(r"^\s*# (?!#)(.*)", markdown)
     if not title:
         raise Exception("Couldn't find any headers in the markdown")
-    if title[0].count("#") != 1:
-        raise Exception("Couldn't find an h1 in the markdown")
-    return title[0].strip("# ")
+    return title[0]
